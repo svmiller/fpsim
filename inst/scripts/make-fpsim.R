52ebf057a1f5e08b9bf8cb1316f463ead6a5a8e1
svmiller <steven.v.miller@gmail.com>
2026-07-04 12:31:03 +0200

    update

--- inst/scripts/make-fpsim.R
+++ inst/scripts/make-fpsim.R
@@ -1,22 +1,29 @@
 library(tidyverse)
 library(qs2)
-library(tictoc)
 
-tic()
-FPSIMASU <- qs_read("docs/data/FPSIMASU.qs")
-FPSIMASW <- qs_read("docs/data/FPSIMASW.qs")
-FPSIMAPKT <- qs_read("docs/data/FPSIMAPKT.qs")
-FPSIMVPK <- qs_read("docs/data/FPSIMVPK.qs")
+FPSIMASU <- qs_read("docs/data/fpsim-alliances-s-unweighted.qs")
+FPSIMASW <- qs_read("docs/data/fpsim-alliances-s-weighted.qs")
+FPSIMAAU <- qs_read("docs/data/fpsim-alliances-a-unweighted.qs")
+FPSIMAAW <- qs_read("docs/data/fpsim-alliances-a-weighted.qs")
+FPSIMAPK <- qs_read("docs/data/fpsim-alliances-pk.qs")
+FPSIMAT <- qs_read("docs/data/fpsim-alliances-taub.qs")
+FPSIMVA <- qs_read("docs/data/fpsim-votes-a.qs")
+FPSIMVPK <- qs_read("docs/data/fpsim-votes-pk.qs")
 
 FPSIMASU %>%
   left_join(., FPSIMASW) %>%
-  left_join(., FPSIMAPKT) %>%
-  full_join(., FPSIMVPK) -> FPSIM
+  left_join(., FPSIMAAU) %>%
+  left_join(., FPSIMAAW) %>%
+  left_join(., FPSIMAPK) %>%
+  left_join(., FPSIMAT) %>%
+  full_join(., FPSIMVA) %>%
+  left_join(., FPSIMVPK) -> FPSIM
+
+FPSIM %>%# names(.)
+  select(ccode1:year, taub, sallyvua, sallybua, sallyvwa, sallybwa,
+         aallyvua, aallybua, aallyvwa, aallybwa,
+         pallyv:kallyb, avoteva:pvotev) -> FPSIM
+
 
 saveRDS(FPSIM, "docs/data/FPSIM.rds")
-toc(log = TRUE) # and, time
 
-sink(file = "inst/scripts/99-fpsim.log")
-timestamp()
-tic.log()
-sink()
